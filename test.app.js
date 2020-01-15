c811775ab95e51d6d88a0b430f69107c1a33d993
cosmocoll <collsitton@gmail.com>
2020-01-14 17:46:22 -0800

    adding numberGuessGame html and js index

--- test.app.js
+++ test.app.js
@@ -1,15 +1,8 @@
 const clickClack = document.getElementById('beginButton');
 const totalScore = document.getElementById('score');
 
-import isDog
-import isJazz
-import isFantasy
 
 clickClack.addEventListener('click', quiz()); 
-
-
-beginButton.addEventListener('click', quiz());
-
 
 function quiz() {
 	
@@ -19,9 +12,35 @@ function quiz() {
 	if (confermation) { 
 	
 		alert('Then let it be so! Mwuahahaha!');
+		
+		
 		const answer1 = prompt('Question 1');
+		
+			 function isDog(str) {
+  if(str.toLowerCase() === 'dog' || str.toLowerCase() === 'Dog') return true;
+  else return false;
+			 }
+
+		
 		const answer2 = prompt('Question 2');
+		
+			function isFantasy(str) {
+  if(str.toLowerCase() === 'Fantasy' || str.toLowerCase() === 'fantasy') return true;
+  else return false;
+			}
+		
 		const answer3 = prompt('Question 3');
+	
+		function isJazz(str) {
+  if(str.toLowerCase() === 'All that jazz' || str.toLowerCase() === 'all that jazz') return true;
+  else return false;
+}
+	
+}
+
+
+		
+		let count = 0
 		
 		if(isDog(answer1)) count += 1;
 		if(isFantasy(answer2)) count += 1;
@@ -34,5 +53,5 @@ function quiz() {
 	}
 }
 
-let count = 0
+
 
