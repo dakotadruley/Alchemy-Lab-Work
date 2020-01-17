50da672142029c46ff4989caa6bc64ceb677c443
cosmocoll <collsitton@gmail.com>
2020-01-17 09:17:42 -0800

    adding updated repos for all files

--- LabThree/rps.js
+++ LabThree/rps.js
@@ -1,43 +1,74 @@
 window.onload=function(){
 
-    import { getRandomThrow, checkResult } from './get-random-throw.js';
+    const button = document.getElementById('my-button');
+    const resetButton = document.getElementById('reset-button');
 
-
-    const resultDisplay = document.getElementById('games');
-    const button = document.getElementById('playSubmit');
-    const resultImage = document.getElementById('');
+    const sumamarySpan = document.getElementById('summary');
+    const resultsDisplay = document.getElementById('result');
     const winsDisplay = document.getElementById('wins');
     const lossesDisplay = document.getElementById('losses');
     const drawsDisplay = document.getElementById('draws');
 
-    let winTotal = 0;
-    let gameTotal = 0;
-    let drawTotal = 0;
+    const rock = document.getElementById('rock');
+    const paper = document.getElementById('paper');
+    const scissors = document.getElementById('scissors');
+
+    let winsCounter = 0;
+    let lossesCounter = 0;
+    let drawsCounter = 0;
+
+    resetButton.addEventListener('click', () => {
+        winsDisplay = 0;
+        lossesDisplay = 0;
+        drawsDisplay = 0;
+        sumamarySpan.classList.add('hidden');
+        
+    })
 
     button.addEventListener('click', () => {
-        alert('works');
+        alert('button works, good news');
 
-        const userInput = document.querySelector('input:checked');
+        const selectedRadioButton = document.querySelector('input:checked');
 
-         const userChoice = userInput.value;
-    //alert(userChoice);
+        const userThrow = selectedRadioButton.value;
 
-        const computerChoice = getRandomThrow();
+        const randomNumber = Math.floor(Math.random() * 3);
 
-        const result = checkResult(userChoice, computerChoice);
+        let number = randomNumber 
+
+        let options = [{rock : 1}, {paper : 2}, {scisssors: 3}];
+
+        switch (userThrow === rock) {
+            case 1: number === option[0].value;
+            return this.console.log('here');
+            break;
+            case 2: number === option[1].value;
+            return this.console.log('here');
+            break;
+            case 3: number === option[2].value;
+            return this.console.log('here');
+            break;
+            default : null;
+        }
+
+        //let rockThrow = rock;
+        //let paperThrow = paper;
+        //let scissorsThrow = scissors;
+
+        //let outcomes = [{rockThrow : 0}, {papaerThrow : 1}, {scissorsThrow : 2}];
+
         
-        if(computerChoice === 'rock')  resultImage.src="https://www.wickedhorror.com/wp-content/uploads/2019/08/The-Rock-864x467.jpg";
+        if(computerThrow === 'rock')  resultImage.src="https://www.wickedhorror.com/wp-content/uploads/2019/08/The-Rock-864x467.jpg";
 
-        if(computerChoice === 'scissors')  resultImage.src="https://www.wweek.com/resizer/86tt-U3ytIrtb7bBYXAIg7XWz7A=/1200x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/wweek/wp-content/uploads/2019/08/30145212/Nicolas-Cage.jpg";
+        if(computerThrow === 'scissors')  resultImage.src="https://www.wweek.com/resizer/86tt-U3ytIrtb7bBYXAIg7XWz7A=/c1200x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/wweek/wp-content/uploads/2019/08/30145212/Nicolas-Cage.jpg";
         
-        if(computerChoice === 'paper') 
+        if(computerThrow === 'paper') 
         resultImage.src="https://cdn.britannica.com/97/194197-050-5BD88874/Vin-Diesel-The-Fast-and-the-Furious.jpg";
   
         if(resluts === 'win') {
             winTotal++;
             wins.textContent = winTotal;
             resultDisplay.classList.remove('hidden');
-            .textContent =
         }
         
 
